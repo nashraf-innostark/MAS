c8a1e028d348f21e5ea84068736977857e38d476
Nouman Ashraf <nashraf@innostark.com>
2012-12-15 16:21:36 +0500

    Sql Helper Class

---
+++ MAS/MAS.DAL/SqlHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+using System.Configuration;
+
+namespace MAS.DAL
+{
+    public static class SqlHelper
+    {
+        public static SqlConnection GetConnection()
+        {
+            string conString = ConfigurationManager.ConnectionStrings["ProjConString"].ConnectionString;
+            return new SqlConnection(conString);
+        }
+    }
+}
--- MAS/MAS/About.aspx
+++ MAS/MAS/About.aspx
@@ -5,7 +5,7 @@
 </asp:Content>
 <asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
     <h2>
-        About asdlf asdfasd f;asd fasdf 
+        About
     </h2>
     <p>
         Put content here.
